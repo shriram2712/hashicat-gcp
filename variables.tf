6094e52337338de84560241b0e430b24c01575c9
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2024-07-15 09:46:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -50,3 +50,12 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "organization" {
+  type = string
+  default = "shriramrajaraman"
+}
+
+variable "workspace" {
+  type = string
+}
